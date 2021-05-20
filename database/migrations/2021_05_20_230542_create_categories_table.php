09beb5d0e7b3c7c445707c7964b46a8fce5cffbe
Nam Luu <nam.luuduc@gmail.com>
2021-05-21 06:45:39 +0700

    migrate cms tables

---
+++ database/migrations/2021_05_20_230542_create_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cms_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('alias')->unique();
+            $table->boolean('enabled')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cms_categories');
+    }
+}
---
+++ database/migrations/2021_05_20_230625_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cms_posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('alias')->unique();
+            $table->boolean('enabled')->default(1);
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('category_id')->constrained('cms_categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cms_posts');
+    }
+}
